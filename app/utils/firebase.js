7e788b988f6a394805dd72890d77d2763b0a991e
elenacarrasco <elenacfnb@gmail.com>
2020-05-12 20:58:30 +0200

    Add firebase config

---
+++ app/utils/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCu1wXcTGJ0BMsVWU9D24a3zwd7tRYCPq0",
+    authDomain: "reedby-app.firebaseapp.com",
+    databaseURL: "https://reedby-app.firebaseio.com",
+    projectId: "reedby-app",
+    storageBucket: "reedby-app.appspot.com",
+    messagingSenderId: "827670430537",
+    appId: "1:827670430537:web:650e4312835033b091d098",
+    measurementId: "G-LT51B3DRWX"
+};
+
+export const firebaseApp = firebase.initializeApp(firebaseConfig);
