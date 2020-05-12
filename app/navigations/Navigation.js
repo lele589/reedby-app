56bbab86507daf0a17978a45949c6a02fe4c9c38
elenacarrasco <elenacfnb@gmail.com>
2020-05-12 20:35:00 +0200

    Add navigation icons (simple-line-icons library)

--- app/navigations/Navigation.js
+++ app/navigations/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
@@ -6,13 +7,24 @@ import BookshelfStack from '../navigations/BookshelfStack'
 import ExploreStack from '../navigations/ExploreStack'
 import AddBookStack from '../navigations/AddBookStack'
 import AccountStack from './AccountStack'
+import { Icon } from 'react-native-elements'
+import { Colors } from '../styles'
 
 const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator
+                initialRouteName="explore"
+                tabBarOptions={{
+                    inactiveTintColor: Colors.grey,
+                    activeTintColor: Colors.green,
+                }}
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ color }) => screenOptions(route, color)
+                })}
+            >
                 <Tab.Screen
                     name="bookshelf"
                     component={BookshelfStack}
@@ -37,3 +49,34 @@ export default function Navigation() {
         </NavigationContainer>
     )
 };
+
+const screenOptions = (route, color) => {
+    let iconName;
+
+    switch (route.name) {
+        case "bookshelf":
+            iconName = "book-open";
+            break;
+        case "explore":
+            iconName = "globe-alt";
+            break;
+        case "add-book":
+            iconName = "plus";
+            break;
+        case "account":
+            iconName = "user";
+            break;
+        default:
+            break;
+    }
+
+    return(
+        <Icon type='simple-line-icon' name={iconName} size={22} color={color} containerStyle={ styles.iconContainer } />
+    )
+};
+
+const styles = StyleSheet.create({
+    iconContainer: {
+        marginTop: 10
+    },
+});
