baef9dbbba9967f66ad5ce9b4ea19adb8ba892b0
elenacarrasco <elenacfnb@gmail.com>
2020-05-17 16:34:48 +0200

    Create common styles

---
+++ app/styles/colors.js
@@ -0,0 +1,12 @@
+// Brand colors
+export const green = '#69b387';
+export const red = '#f77171';
+
+// Greyscale
+export const white = '#fff';
+export const black = '#000';
+export const grey = '#848282';
+export const greyDark = '#3A3E3D';
+export const greyLight = '#aeacac';
+export const greyLight2 = '#dddddd';
+export const greySoft = '#f4f4f4';
