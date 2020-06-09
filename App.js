85cb08ff75d8208a5ac752c14b6d7d03db0e80dc
elenacarrasco <elenacfnb@gmail.com>
2020-06-09 17:34:07 +0200

    Add custom Google Font + AppLoading component

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import React from 'react';
-
+import { AppLoading } from 'expo';
 import Navigation from "./app/navigations/Navigation";
 import { firebaseApp } from "./app/utils/firebase";
 
+import {
+  useFonts,
+  Jost_300Light,
+  Jost_400Regular,
+  Jost_500Medium,
+} from '@expo-google-fonts/jost';
+
 export default function App() {
-  return (
-      <Navigation/>
-  );
+  let [fontsLoaded] = useFonts({
+    Jost_300Light,
+    Jost_400Regular,
+    Jost_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+        <Navigation/>
+    );
+  }
 }
