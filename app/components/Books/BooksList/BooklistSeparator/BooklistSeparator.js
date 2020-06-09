622008126c0f5099a2d094919cd8bd65e6291e27
elenacarrasco <elenacfnb@gmail.com>
2020-06-09 17:35:44 +0200

    Create BookListSeparator Component

---
+++ app/components/Books/BooksList/BooklistSeparator/BooklistSeparator.js
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import React from "react";
+
+import { styles } from "./styles";
+
+export default function BookListSeparator () {
+    return (
+        <View style={styles.separator}/>
+    );
+}
---
+++ app/components/Books/BooksList/BooklistSeparator/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+import { Colors } from "../../../../styles";
+
+export const styles = StyleSheet.create({
+    separator: {
+        height: 1,
+        width: "100%",
+        backgroundColor: Colors.greySoft,
+    },
+});
